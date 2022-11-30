2b3bfd2bec8b429f945ed5be313ebf649f254e96
unknown <babumanish0808@gmail.com>
2022-11-30 21:23:47 +0530

    login

---
+++ login.js
@@ -0,0 +1,34 @@
+1.Prime problem
+let c=0
+let a=17;
+for(i=1;i<=17;i++){
+     if(a%i==0){
+     
+    }
+ }
+if(c==2){
+console.log("Yes")
+}
+else{
+console.log("No")
+}
+
+
+2.Palindrome
+let a="naman";
+let b="";
+for(i=a.length-1;i>0;i--){
+    b=b+a[i];
+ }
+let c=true
+for(j=0;j<a.length;j++){
+     if(a[j]==b[j]){
+         
+      }    
+}
+if(c==true){
+console.log("Yes")
+}
+else{
+console.log("No")
+}
